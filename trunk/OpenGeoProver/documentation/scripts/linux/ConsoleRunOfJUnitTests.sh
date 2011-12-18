c83cea9d544d5c09531d62ae184a85271b0e2622
ivan.petrov1@gmail.com <ivan.petrov1@gmail.com@a84972d1-4f98-7bfa-e166-07b7cf2a6a4b>
2011-12-18 01:51:23 +0000

    1. Fix for "Issue#1": fixed [junit] task from Ant build file. 2. Enabled <javadoc> task 3. Fixed scripts for console running of JUnit tests - fixed project path.

--- trunk/OpenGeoProver/documentation/scripts/linux/ConsoleRunOfJUnitTests.sh
+++ trunk/OpenGeoProver/documentation/scripts/linux/ConsoleRunOfJUnitTests.sh
@@ -3,10 +3,10 @@
 # Call JUnit test
 
 # Plain TestRunner
-java -cp ../build/jutest;../3rd_party_libs/junit-4.9b2.jar;../3rd_party_libs/log4j-1.2.16.jar junit.textui.TestRunner com.ogp.test.junit.JUTestPolynomialsSuite
+java -cp ../build/jutest;../3rd_party_libs/junit-4.9b2.jar;../3rd_party_libs/log4j-1.2.16.jar junit.textui.TestRunner com.ogprover.test.junit.JUTestPolynomialsSuite
 
 # SwingGUI TestRunner
-java -cp ../build/jutest;../3rd_party_libs/junit-3.8.2.jar;../3rd_party_libs/log4j-1.2.16.jar junit.swingui.TestRunner com.ogp.test.junit.JUTestPolynomialsSuite
+java -cp ../build/jutest;../3rd_party_libs/junit-3.8.2.jar;../3rd_party_libs/log4j-1.2.16.jar junit.swingui.TestRunner com.ogprover.test.junit.JUTestPolynomialsSuite
 
  # OTHER EXAMPLES ...
  
