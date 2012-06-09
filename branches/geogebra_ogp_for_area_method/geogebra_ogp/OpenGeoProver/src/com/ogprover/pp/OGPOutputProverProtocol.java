4e0f9d45d9c4c3047177eb5da57d61bcccc86ca0
ivan.petrovic.matf@gmail.com <ivan.petrovic.matf@gmail.com@a84972d1-4f98-7bfa-e166-07b7cf2a6a4b>
2012-06-09 15:57:54 +0000

    Issue #19: Modifying classes for OGP API and prover protocols to be base for similar classes that will be used as a connection between various external systems and OGP. <== Area method branch.

--- branches/geogebra_ogp_for_area_method/geogebra_ogp/OpenGeoProver/src/com/ogprover/pp/OGPOutputProverProtocol.java
+++ branches/geogebra_ogp_for_area_method/geogebra_ogp/OpenGeoProver/src/com/ogprover/pp/OGPOutputProverProtocol.java
@@ -7,28 +7,22 @@ package com.ogprover.pp;
 /**
  * <dl>
  * <dt><b>Class description:</b></dt>
- * <dd>Class for output prover protocol</dd>
+ * <dd>Abstract class for output prover protocol</dd>
  * </dl>
  * 
  * @version 1.00
  * @author Ivan Petrovic
  */
-public class OGPOutputProverProtocol extends OGPProverProtocol {
+public abstract class OGPOutputProverProtocol implements OGPProverProtocol {
 	/*
-	 * TODO
-	 * 
-	 * Output prover protocol should contain following information:
+	 * Output prover protocol is used to pass prover results from OGP
+	 * to some external system. It should contain following information:
 	 * 
 	 * 1. Brief prover result - whether theorem has been proved or
 	 * 		disproved or couldn't be proved; with time and space
 	 * 		measures of execution.
 	 * 2. Generated output reports and path of log file.
-	 * 3. Collection of all NDG condition in form derived from algebraic
-	 * 		form so that caller could understand them. The best way for 
-	 * 		doing this is to pass collection of statements that represent
-	 * 		degenerative conditions. E.g. if some NDG condition means that
-	 * 		two points must not be identical, then collection will have
-	 * 		the statement about two identical points. 
+	 * 3. Collection of all NDG conditions in user readable form. 
 	 */
 	
 	/*
@@ -62,22 +56,10 @@ public class OGPOutputProverProtocol extends OGPProverProtocol {
 	}
 	
 	
-	
-	
-	/*
-	 * ======================================================================
-	 * ========================== CONSTRUCTORS ==============================
-	 * ======================================================================
-	 */
-	
-	
-	
 
 	/*
 	 * ======================================================================
 	 * ========================== SPECIFIC METHODS ==========================
 	 * ======================================================================
-	 */
-
-	
+	 */	
 }
