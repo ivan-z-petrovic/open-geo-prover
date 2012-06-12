dd046a75dcf64b78d7e68f7f82e6913b9bdb69f1
ddfontaines <ddfontaines@a84972d1-4f98-7bfa-e166-07b7cf2a6a4b>
2012-06-12 21:54:34 +0000

    Creation of the construction classes for the area method. The AMRatio class is temporary, I will see in the next few days if I can use another already-existing class.

---
+++ geogebra_ogp/OpenGeoProver/src/com/ogprover/pp/tp/geoconstruction/AMRatio.java
@@ -0,0 +1,12 @@
+/* 
+ * DISCLAIMER PLACEHOLDER 
+ */
+package com.ogprover.pp.tp.geoconstruction;
+
+/**
+ * Temporary class, which will after discussion be either deleted or written.
+ */
+public class AMRatio {
+	private int r;
+
+}
