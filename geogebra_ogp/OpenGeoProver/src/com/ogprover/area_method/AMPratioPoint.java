6d5b97c337ee708c9ffa8b62376c360b7e06d134
ddfontaines <ddfontaines@a84972d1-4f98-7bfa-e166-07b7cf2a6a4b>
2012-06-12 16:32:47 +0000

    Deletion of an useless parameter in TRatio & PRatio constructor.

--- geogebra_ogp/OpenGeoProver/src/com/ogprover/area_method/AMPratioPoint.java
+++ geogebra_ogp/OpenGeoProver/src/com/ogprover/area_method/AMPratioPoint.java
@@ -33,7 +33,7 @@ public class AMPratioPoint extends AMPoint {
 	/**
 	 * Points used to construct this point
 	 */
-	protected AMPoint y,w,u,v;
+	protected AMPoint w,u,v;
 	/**
 	 * Ratio used to construct this point
 	 */
@@ -51,21 +51,18 @@ public class AMPratioPoint extends AMPoint {
 	 * See http://hal.inria.fr/hal-00426563/PDF/areaMethodRecapV2.pdf
 	 * 
 	 * @param label 	Label of point
-	 * @param y			Previously constructed point
 	 * @param w			Previously constructed point
 	 * @param u			Previously constructed point
 	 * @param v 		Previously constructed point
 	 * @param r			Ratio
 	 */
-	public AMPratioPoint(String label,AMPoint y,AMPoint w,AMPoint u, AMPoint v, AMRatio r) {
+	public AMPratioPoint(String label,AMPoint w,AMPoint u, AMPoint v, AMRatio r) {
 		type = 2;
 		this.label = label;
-		this.y = y;
 		this.w = w;
 		this.u = u;
 		this.v = v;
 		dependantPoints = new Vector<AMPoint>();
-		dependantPoints.add(y);
 		dependantPoints.add(w);
 		dependantPoints.add(u);
 		dependantPoints.add(v);
@@ -76,21 +73,18 @@ public class AMPratioPoint extends AMPoint {
 	 * Returns the point with an automatically generated label, constructed as PRATIO(y,w,u,v,r)
 	 * See http://hal.inria.fr/hal-00426563/PDF/areaMethodRecapV2.pdf
 	 * 
-	 * @param y		Previously constructed point
 	 * @param w		Previously constructed point
 	 * @param u		Previously constructed point
 	 * @param v		Previously constructed point
 	 * @param r		Ratio
 	 */
-	public AMPratioPoint(AMPoint y,AMPoint w,AMPoint u, AMPoint v, AMRatio r) {
+	public AMPratioPoint(AMPoint w,AMPoint u, AMPoint v, AMRatio r) {
 		type = 2;
 		this.label = nextAvailableName();
-		this.y = y;
 		this.w = w;
 		this.u = u;
 		this.v = v;
 		dependantPoints = new Vector<AMPoint>();
-		dependantPoints.add(y);
 		dependantPoints.add(w);
 		dependantPoints.add(u);
 		dependantPoints.add(v);
